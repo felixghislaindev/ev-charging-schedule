831597254ca6a0b2623d6aa527a0da893d41ad24
Felix ghislain <felixghislain@Felixs-MacBook-Pro.local>
2023-12-04 15:17:02 +0000

    basic setup

---
+++ components/ChargingInfo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChargingInfo = () => {
+  return <div></div>;
+};
+
+export default ChargingInfo;
---
+++ components/PostalCodeInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostalCodeInput = () => {
+  return <div></div>;
+};
+
+export default PostalCodeInput;
